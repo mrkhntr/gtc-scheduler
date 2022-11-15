dd18d59b9fc73525eed8de2f812986b6ba9fb3f1
Mark Hunter <hunter.mark@husky.neu.edu>
2022-11-14 21:39:22 -0800

    first iteration

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    How to use: This app only works for pre-approved usernames. It is for 
+    auto booking of a court at SF GTC.
+    <br />
+    To use it you need to know your login and userId for the person you 
+    are booking to court on to find your userId login to GTC portal,
+    open <a href="https://medium.com/@ConnorFinnegan/how-to-effectively-inspect-network-activity-in-chrome-dev-tools-fd18592a735a#:~:text=feature%20in%20Chrome.-,Getting%20Started,-To%20access%20this">network tools</a>
+    and select your name again from the dropdown. You should see "switch-linked-user?", id= is the UserId you selected.
+    <br />
+    Its important to keep tab open and computer not asleep for it to trigger at
+    time specified. You can test by not toggling "Actually Make Reservation".
+  </div>
+  <VueCountdown v-if="countdown !== null" :time="countdown"  :interval="100" v-slot="{ totalSeconds }"  @end="onCountdownEnd">
+    Time Remaining： {{ totalSeconds }} seconds.
+  </VueCountdown>
+  <iframe v-if="response" :srcdoc="response" />
+  <FormGTC @requestTime="onSubmit"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormGTC from './components/FormGTC.vue'
+import VueCountdown from '@chenfengyuan/vue-countdown'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormGTC,
+    VueCountdown,
+  },
+  data() {
+    return {
+      error: null,
+      countdown: null,
+      toSend: null,
+      response: null,
+    }
+  },
+
+  methods: {
+    onSubmit(values) {
+      this.toSend = null;
+      this.response = 'Attempting to make reservation'
+      console.log('herer', values);
+      this.countdown = values.executeAt ? (new Date(values.executeAt) - new Date()) : 0;
+      console.log({ countdown: this.countdown, date: Math.floor(new Date(values.timeToReserveDateTime).getTime() / 1000)});
+      const data = {
+          username: values.username,
+          password: values.password,
+          userId: values.userId,
+          intervalInMins: values.intervalInMins,
+          timeToReserveSeconds: Math.floor(new Date(values.timeToReserveDateTime).getTime() / 1000),
+          locationToReserve: values.locationToReserve,
+          isConfirmed: values.isConfirmed,
+        };
+      console.log('Data to be sent', data);
+      this.toSend = () => axios.post(
+        'https://uglte8qxlf.execute-api.us-east-1.amazonaws.com/dev/gtc/reserve', 
+        data
+      )
+    },
+    async onCountdownEnd() {
+      try {
+        console.log('attempting to send');
+        const result = await this.toSend();
+        alert('sent sucessfully, see result below');
+        console.log(result);
+        this.response = result.data;
+      } catch (error) {
+        alert(`Sending the request resulted in systamic error ${error.message}`)
+      }
+
+    }
   }
 }
 </script>
@@ -19,7 +81,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/FormGTC.vue
@@ -0,0 +1,130 @@
+<template>
+    <FormKit
+        type="form"
+        submit-label="Save"
+        @submit="saveMe"
+      >
+        <FormKit
+            type="datetime-local"
+            label="Time to execute"
+            name="executeAt"
+        />
+        <FormKit
+            type="text"
+            label="username"
+            name="username"
+            validation="required"
+            help="Only approved usernames will work"
+        />
+        <FormKit
+            type="password"
+            label="password"
+            name="password"
+            validation="required"
+        />
+        <FormKit
+            type="text"
+            label="userId"
+            name="userId"
+            validation="required|number"
+            help="This is not login username, it is the userId to be used for reservation"
+        />
+        <FormKit
+            type="radio"
+            name="intervalInMins"
+            label="Interval In Mins"
+            :options="[30, 45, 60, 90]"
+        />
+        <FormKit
+            type="radio"
+            label="Location to Reserve"
+            name="locationToReserve"
+            :options="{ 
+                1: 'Tennis', 
+                35: 'Pickleball / Mini Tennis',
+            }"
+        />
+        <FormKit
+            type="datetime-local"
+            label="Reserve Court Time"
+            name="timeToReserveDateTime"
+            validation="required"
+        />
+        <FormKit
+            :value="false"
+            type="checkbox"
+            name="isConfirmed"
+            label="Actually Make Reservation (over not reserving/testing)"
+        />
+    </FormKit>
+</template>
+
+<script>
+
+
+export default {
+    name: 'FormGTC',
+    data() {
+      return {
+        error: null,
+        countdown: null,
+        toSend: null,
+      }
+    },
+    methods: {
+      //values, {setSubmitting, setErrors, setStatus, resetForm}
+        async saveMe(values) {
+          console.log(values)
+          this.$emit('requestTime', values);
+        }
+    }
+}
+</script>
+
+
+<style>
+li {
+   list-style-type:none;
+}
+p {
+  font-size: 0.9em;
+  color: #646464;
+  line-height: 1.5;
+}
+h1 {
+  margin-top: 0;
+}
+h2 {
+  color: green;
+}
+hr {
+  display: block;
+  height: 1px;
+  margin: 1.5em 0;
+  border: 0;
+  background-color: #e4e4e4;
+}
+.formkit-form {
+  width: 420px;
+  padding: 1.5em;
+  border: 1px solid #e4e4e4;
+  border-radius: 1em;
+  margin: 0 auto 1em auto;
+}
+.hide {
+  display: none;
+}
+#registration-example pre {
+  margin-bottom: 10px;
+}
+@media (min-width: 400px) {
+  .double {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .double > .formkit-outer {
+    width: calc(50% - 0.5em);
+  }
+}
+</style>
